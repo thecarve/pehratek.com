075773e62aa4eafca39b1a07a3c0fc114ff73c99
jugchock <jugchock@gmail.com>
2014-02-12 16:37:27 -0600

    Test config.

--- karma.conf.js
+++ karma.conf.js
@@ -17,10 +17,11 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/scripts/*.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js'
+//      'app/scripts/*.js',
+//      'app/scripts/**/*.js',
+//      'test/mock/**/*.js',
+//      'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
